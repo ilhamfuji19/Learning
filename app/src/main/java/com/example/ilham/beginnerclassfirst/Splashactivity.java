ca318e8ca69ea3e798a4d920679a33b0d7f6334c
ilhamfuji19 <ilhamfuji19@gmail.com>
2016-11-12 15:10:56 +0700

    input all

---
+++ app/src/main/java/com/example/ilham/beginnerclassfirst/Splashactivity.java
@@ -0,0 +1,21 @@
+package com.example.ilham.beginnerclassfirst;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splashactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashactivity);
+
+    Thread timer =new Thread(){
+        public void run(){
+        }
+    }
+
+
+
+    }
+}
